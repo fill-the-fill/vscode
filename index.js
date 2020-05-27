90cd6310181b8efa25c527f07f08c6190cb0b1c9
fill ick <filjuwabbyo@gmail.com>
2020-05-27 14:05:27 +0300

    made some changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const hadlebars = require('handlebars')
+
 const DUMMY = function(){
+
     let names = "Josh";
+
     alert('A');
 }; DUMMY();
 
 alert('B');
 
-const Rooby = function(){
+// Econst Rooby() = function(){
+
     let names = "Bob";
     alert('A');
+
 }; Rooby();
 
 Rooby
@@ -17,11 +23,12 @@ alert('B');
 const sadasf = function() {
     const sa = function() {
         {
-    let name = "Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob "
+
+    let name = "Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob "
         }
     }
-}
 
+}
 
 
 

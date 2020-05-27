face347f0cc6bdbdf15a8218ca8ad1f74869334d
fill ick <filjuwabbyo@gmail.com>
2020-05-27 10:27:45 +0300

    hello

---
+++ main.js
@@ -0,0 +1,27 @@
+let qweqweq = function() {
+    let dummy = function() {
+    
+        let something = [qweqweq];
+        something.push;
+        
+        let rooby = function() {
+            let baba = 123;
+            return rooby;
+        }
+    } 
+}
+
+let dummy = function() {
+    let dommy = function() {
+        
+        let something = [dommy];
+        something.push;
+        
+        let rooby = function() {
+            let baba = 123;
+            return rooby;
+        }
+    } 
+}
+
+

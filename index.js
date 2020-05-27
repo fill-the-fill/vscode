be08afde47baa7dbe1e48c7ae87e5a724165953d
fill ick <filjuwabbyo@gmail.com>
2020-05-27 13:29:09 +0300

    made some changes

---
+++ index.js
@@ -0,0 +1,27 @@
+const DUMMY = function(){
+    let names = "Josh";
+    alert('A');
+}; DUMMY();
+
+alert('B');
+
+const Rooby = function(){
+    let names = "Bob";
+    alert('A');
+}; Rooby();
+
+Rooby
+
+alert('B');
+
+const sadasf = function() {
+    const sa = function() {
+        {
+    let name = "Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob Bob "
+        }
+    }
+}
+
+
+
+
